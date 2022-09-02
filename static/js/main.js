afe751b020834b933dc5f160dfca342666c1e2d4
lfh1947 <111464870+lfh1947@users.noreply.github.com>
2022-09-01 18:01:13 -0700

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -14,7 +14,7 @@ console.log("just before single marker above dededot");
  
 
 const myIcon = L.icon({
-    iconUrl: 'http://leafletjs.com/examples/custom-icons/leaf-green.png',
+    iconUrl: 'http://leafletjs.com/examples/custom-icons/leaf-red.png',
     iconSize: [18, 46],
     iconAnchor: [10, 45],
     popupAnchor: [-3, -76]
