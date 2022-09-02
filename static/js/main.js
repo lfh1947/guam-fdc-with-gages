9c74b211f229bafbe2a5076fc240e15dede2d64d
lfh1947 <111464870+lfh1947@users.noreply.github.com>
2022-09-02 15:25:07 -0700

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -16,8 +16,8 @@ const myIcon = L.icon({
     iconUrl: 'https://lfh1947.github.io/guam-fdc-with-gages/static/js/red google icon.png',
     //iconUrl: '/placeholder.png',
     // iconUrl: 'http://leafletjs.com/examples/custom-icons/leaf-red.png',
-    iconSize: [24, 24],
-    iconAnchor: [12, 23],
+    iconSize: [36, 36],
+    iconAnchor: [18, 35],
     popupAnchor: [-3, -76]
       //shadowUrl: 'my-icon-shadow.png',
       //shadowSize: [68, 95],
